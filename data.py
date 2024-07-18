6ccd4a6870139852a66a1ca5f4559aee55b56df4
Fachrul Ramdaniar Hidayatno <adevillz14@gmail.com>
2024-07-18 11:03:56 +0700

    new

---
+++ data.py
@@ -0,0 +1,43 @@
+answer = {
+    'Apa itu Santiana NC ?':
+        'Santiana Nutrition Club (Santiana NC) adalah bagian dari perusahaan Herbalife, sebuah komunitas yang diakui dalam bidang kesehatan dan kebugaran. '
+        'Komunitas ini didirikan dengan tujuan mendukung customer dalam menerapkan pola hidup yang sehat dan aktif',
+
+    'Apa saja solusi yang ditawarkan Santiana NC ?':
+        'Kami mengundang Anda untuk bergabung dalam program kami yang dapat membantu dan memberikan solusi terkait mencapai berat badan ideal dan pola hidup sehat.',
+
+    'Program apa yang dapat dibantu oleh Santiana Nutrition Club ?': f"""Santiana NC dapat memberikan solusi terkait :
+    \t• Sarapan Pagi Sehat & Bergizi 
+    \t• Naik/Turun Berat Badan
+    \t•	Nutrisi Untuk Pemulihan Keksehatan
+    \t•	Mengecilkan Perut Buncit
+    \t•	Awet Muda & Penampilan Oke
+    \t•	Meningkatkan Stamina & Vitalitas
+    \t•	Informasi Pola Makan Sehat\n
+*Jutaan orang terlah berhasil dengan program ini*
+    """,
+
+    'Menu Program Santiana NC': f"""Makanan terbaik untuk Anda yang aktif, gaya hidup sehat.\ndengan kalori rendah, tanpa tambahan gula, tinggi serat, tinggi protein, kaya akan vitamin dan mineral, juga 100% bahan alami, diantaranya :
+    \t• Antioxidant Tea
+    \t(Hight antioxidants to increase metabolism and stamina) 
+    \t• Berry Mint
+    \t(High antioxidant tea with Strawberry and mint leaf)
+    \t•	Lychee Lemon
+    \t(High antioxidant tea with Lychee and lemon Slices)
+    \t•	Pineapple Mint
+    \t(Hight antioxidant tea with pineapple and mint leaf)
+    """,
+
+    'Menu Program Santiana Nutri Sharkepro': f"""Menu Program Santiana Nutri Sharkepro, diantaranya :
+    \t• Antioxidant Tea
+    \t(Shake protein with taro flavour and some topping)
+    \t• Nutri-Berry
+    \t(Shake protein with berry flavour and some topping)
+    \t•	Lychee Lemon
+    \t(High antioxidant tea with Lychee and lemon Slices)
+    \t•	Nutri-Mango
+    \t(Shake protein with mango flavor and some topping)
+    """,
+
+    'Back': 'back',
+}
